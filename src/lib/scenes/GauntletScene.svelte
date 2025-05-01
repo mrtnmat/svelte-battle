a676cb16dda096d933f2071a93899f1e4ece13c3
Matteo <mrtn.matteo@proton.me>
2025-05-01 21:30:30 +0200

    removed gamestate.js

--- src/lib/scenes/GauntletScene.svelte
+++ src/lib/scenes/GauntletScene.svelte
@@ -1,9 +1,54 @@
 <script>
-  import { createInitialState } from "../GameState.js";
+  import { moveList } from "../Moves.js";
   import PokemonCard from "../components/PokemonCard.svelte";
   import BattleLog from "../components/BattleLog.svelte";
   import { executeTurn, selectRandomMove } from "../BattleMechanics.js";
   import { changeScene, SCENES } from './SceneManager.svelte.js';
+  
+  // Create initial Pokemon templates
+  function createPikachu() {
+    return {
+      name: "Pikachu",
+      level: 15,
+      hp: 35,
+      maxHp: 35,
+      attack: 21,
+      defense: 17,
+      speed: 32,
+      moves: {
+        'Move 1': {
+          ...moveList["Tackle"],
+          ppRemaining: 10,
+        },
+        'Move 2': {
+          ...moveList["Thundershock"],
+          ppRemaining: 20,
+        },
+      }
+    };
+  }
+
+  function createBulbasaur() {
+    return {
+      name: "Bulbasaur",
+      level: 5,
+      hp: 19,
+      maxHp: 19,
+      attack: 9,
+      defense: 9,
+      speed: 9,
+      moves: {
+        'Move 1': {
+          ...moveList["Tackle"],
+          ppRemaining: 20,
+        },
+        'Move 2': {
+          ...moveList["Vine Whip"],
+          ppRemaining: 20,
+        },
+      }
+    };
+  }
   
   // Gauntlet state keeps track of progress across battles
   let gauntletState = $state({
@@ -19,9 +64,8 @@
   initGauntlet();
   
   function initGauntlet() {
-    // Create initial player Pokémon from initial state
-    const initialState = createInitialState();
-    gauntletState.playerPokemon = structuredClone(initialState.pokemon1);
+    // Create initial player Pokémon
+    gauntletState.playerPokemon = createPikachu();
     gauntletState.defeatedCount = 0;
     
     // Start the first battle
@@ -29,9 +73,8 @@
   }
   
   function generateNextBattle() {
-    // Create the same enemy for each battle (Bulbasaur from initial state)
-    const initialState = createInitialState();
-    const enemyPokemon = structuredClone(initialState.pokemon2);
+    // Create the enemy Pokémon
+    const enemyPokemon = createBulbasaur();
     
     // Set up battle state
     battleState = {
